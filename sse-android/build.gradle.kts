4c191b28bee9d67f3d7ee6a0fef376c2dad65073
Hans <121116111@qq.com>
2026-04-11 21:36:37 +0800

    chore: remove native android and ios demo projects

--- sse-android/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.android) apply false
-  // Compose plugin not available on Kotlin 1.8.22; handled by composeOptions in module if needed
-  // alias(libs.plugins.kotlin.compose) apply false
-  alias(libs.plugins.android.library) apply false
-}
